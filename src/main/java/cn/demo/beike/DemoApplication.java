2958ff68a42036a1eeed5666a1e62a05379f8da7
gxl-ops <xinleguo@outlook.com>
2020-03-20 18:56:20 +0800

    demo还原

--- src/main/java/cn/demo/beike/DemoApplication.java
+++ src/main/java/cn/demo/beike/DemoApplication.java
@@ -28,34 +28,31 @@ public class DemoApplication {
      * @return
      */
 
-//    @Bean
-//    public TomcatServletWebServerFactory tomcatServletWebServerFactory(Connector connector){
-//        TomcatServletWebServerFactory tomcat=new TomcatServletWebServerFactory(){
-//            @Override
-//            protected void postProcessContext(Context context) {
-//                SecurityConstraint securityConstraint=new SecurityConstraint();
-//                securityConstraint.setUserConstraint("CONFIDENTIAL");
-//                SecurityCollection collection=new SecurityCollection();
-//                collection.addPattern("/*");
-//                securityConstraint.addCollection(collection);
-//                context.addConstraint(securityConstraint);
-//            }
-//        };
-//        tomcat.addAdditionalTomcatConnectors(connector);
-//        return tomcat;
-//    }
-//
-//    @Bean
-//    public Connector connector(){
-//        System.out.println("创建了自定义的http->https的connector");
-//        Connector connector=new Connector("org.apache.coyote.http11.Http11NioProtocol");
-//        connector.setScheme("http");
-//        connector.setPort(8088);
-//        connector.setSecure(false);
-//        connector.setRedirectPort(443);
-//        return connector;
-//    }
+    @Bean
+    public TomcatServletWebServerFactory tomcatServletWebServerFactory(Connector connector){
+        TomcatServletWebServerFactory tomcat=new TomcatServletWebServerFactory(){
+            @Override
+            protected void postProcessContext(Context context) {
+                SecurityConstraint securityConstraint=new SecurityConstraint();
+                securityConstraint.setUserConstraint("CONFIDENTIAL");
+                SecurityCollection collection=new SecurityCollection();
+                collection.addPattern("/*");
+                securityConstraint.addCollection(collection);
+                context.addConstraint(securityConstraint);
+            }
+        };
+        tomcat.addAdditionalTomcatConnectors(connector);
+        return tomcat;
+    }
 
-
-
+    @Bean
+    public Connector connector(){
+        System.out.println("创建了自定义的http->https的connector");
+        Connector connector=new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setScheme("http");
+        connector.setPort(8088);
+        connector.setSecure(false);
+        connector.setRedirectPort(443);
+        return connector;
+    }
 }
